7aba35104f915cf818c1866b7bacf40f685938a8
Soheil <soheil.nemati@outlook.com>
2021-03-26 15:23:09 -0600

    Added some changes to make things cleaner

--- GradientDescent.py
+++ GradientDescent.py
@@ -10,15 +10,12 @@ from numpy import linalg as LA
 import unittest
 
 
-# In[27]:
+# In[2]:
 
 
 
 class GradientDescent:
     
-        
-    def __init__(self):
-        pass
     
     def gradient(self,function,x,delta_val):
         """
@@ -27,11 +24,15 @@ class GradientDescent:
         delta_val: this is refering to the delta value in calculating the gradient
             for example for 1 dimenstion delta:= d in (f(x+d)-f(x))/d   
         """
-        delta = delta_val*np.eye(len(x))# defining len(x) dimention with value of delta_val in the diognal to calculate the gradient
+        n=len(x)
+        delta = delta_val*np.eye(n)# defining len(x) dimention with value of delta_val in the diognal to calculate the gradient
         
-        return np.array([ (function(x+delta[i])-function(x))/delta_val for i in range(len(x))])
+        return np.array([ (function(x+delta[i])-function(x))/delta_val for i in range(n)])
         
-    def gradientDescent(self, function, initial_point, iterations, learning_rate, delta_val):
+    def gradientDescent(self, function, initial_point, iterations=10000, learning_rate=0.1, delta_val=0.01):
+        
+        if type(initial_point)!=np.ndarray:
+            raise ValueError("Only accepting ndarrays as input, please update your function")
         theta=initial_point
         for i in range(iterations):
             theta=theta-learning_rate*self.gradient(function,theta,delta_val)
@@ -40,39 +41,35 @@ class GradientDescent:
     
 
 
-# In[43]:
+# In[4]:
 
 
 class Test(unittest.TestCase):
     
-    # This is a variable to generate normal train set with train_size size
-    #increasing or decreasing it may effect the test
-    train_size=5000
-    
-    
-    # This is a variable to generate huge train set with train_size size
-    #increasing this will effect the time of the tests
-    efficiency_train_size=1000000
-    
-    batchsize_eff=100000
-    
-    def generate_random_noraml_point(self,count,pointCount):
-        point=[[(i+count)] for i in range(pointCount)]
-        x_train=[[i] for i in range(count)]
-        y_train=[[i] for i in range(count)]+np.random.randn(count, 1)
-        return point,x_train,y_train 
+    iterations=500
+    efficiency_iterations=100000
     
     def f(self,x):
         return x[0]*x[0]+2*x[1]*x[1]
     
-    def test_fitLine(self):
-        point,x_train,y_train=self.generate_random_noraml_point(self.train_size,1)
+    def test_gradient(self):
         gd=GradientDescent()
         gradient=gd.gradient(self.f,[3,2],0.01)
         assert LA.norm(gradient-[6 ,8]) < 0.03
-        min_0=gd.gradientDescent(self.f,np.array([5,5]),200,0.1,0.0001)
-        assert LA.norm(min_0-[0 ,0]) < 0.03
         
+    def test_gradientDescent(self):
+        gd=GradientDescent()
+        min_val=gd.gradientDescent(self.f,np.array([5,5]),self.iterations,0.1,0.0001)
+        assert LA.norm(min_val-[0 ,0]) < 0.03
+        
+    def test_gradientDescent_eff(self):
+        gd=GradientDescent()
+        min_val=gd.gradientDescent(self.f,np.array([20,20]),self.efficiency_iterations,0.1,0.0001)
+        assert LA.norm(min_val-[0 ,0]) < 0.03
+    def test_ValueError(self):
+        gd=GradientDescent()
+        with self.assertRaises(ValueError) as context:
+            min_val=gd.gradientDescent(self.f,5)
     
         
         
